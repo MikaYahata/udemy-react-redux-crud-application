f4b7f2681cd085d09a3a03d298029be42495bb2d
m.yahata <m.yahata@jmtech.co.jp>
2020-12-09 11:29:16 +0900

    Learn props of component

--- src/App.js
+++ src/App.js
@@ -22,20 +22,32 @@ import React from 'react';
 //   )
 // }
 
+/*Props 属性（メンバ）定義機能*/
 const App = () => {
+  const profiles = [
+    {name: "Taro", age:10},
+    {name: "Hanako", age:5},
+    {name: "NoName"}
+  ];
+
   return  (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return  <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return  <div>Meow!</div>
+const User = (props) => {
+  return  <div>Hi, Iam {props.name}, and {props.age} years old! </div>
 }
 
+/*defaultProps デフォルト値定義機能*/
+User.defaultProps = {
+  age: 1
+}
 
 export default App;
